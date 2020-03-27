c324f1f9444c9a0a49d0038467a9ed46b6db68e6
Daniel Merys <dkmerys@gmail.com>
2020-03-27 10:36:07 -0700

    create six if/else branches

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
   $("#form-group").submit(function(event){
   var question1 = $("input:radio[name=question1]:checked").val();
-  // var question2 = $("input:radio[name=question2]:checked").val();
-  // var question3 = $("input:radio[name=question3]:checked").val();
-  // var question4 = $("input:radio[name=question4]:checked").val();
-  // var question5 = $("input:radio[name=question5]:checked").val();
+  var question2 = $("input:radio[name=question2]:checked").val();
+  var question3 = $("input:radio[name=question3]:checked").val();
+  var question4 = $("input:radio[name=question4]:checked").val();
+  var question5 = $("input:radio[name=question5]:checked").val();
   
-  if (question1 = "computer") {
-    answer === "C#"
-    alert("worked!")
+  if (question5 === "sports" && question4 === "villain"){
+    answer = "Python"
+  } else if (question1 === "computer" && question3 === "noweb") {
+    answer = "C#"
+  } else if (question3 === "web" && question2 === "iphone"){
+    answer = "Swift"
+  } else if (question2 === "iphone") {
+    answer = "Swift"
+  } else if (question1 === "computer") {
+    answer = "C#"
+  } else if (quesiton5 === "sports") {
+    answer = "Python"
   }
+  
 
   $("#answer").empty().append(answer);
     $("#answer-box").show();
